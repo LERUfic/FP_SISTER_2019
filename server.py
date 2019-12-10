a2f4fbc98f89c00e62063502f520360c94802653
raldo <raldo0104@gmail.com>
2019-12-11 01:05:10 +0700

    try to fix bug

--- server.py
+++ server.py
@@ -39,7 +39,9 @@ class FileServer(object):
             with open('board_playerdata.db','rb') as f:
                 self.boardplayer = pickle.load(f)
         else:
-            self.boardplayer = [[None for j in range(9)] for i in range(9)]
+            self.boardplayer = [[None,None],[None,None],[None,None],
+                                [None,None],[None,None],[None,None],
+                                [None,None],[None,None],[None,None]]
             with open('board_playerdata.db','wb') as f:
                 pickle.dump(self.boardplayer,f, pickle.HIGHEST_PROTOCOL)
 
